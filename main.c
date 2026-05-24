aa5ca71956fda048d59a8fe59d098a13165a6bcd
saadmann <mtaslimaroja@gmail.com>
2026-05-24 16:15:07 +0600

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "raycast.h"
+
+int main(int argc, char* argv[]) {
+    printf("Starting DOOM-Style Raycaster...\n");
+    
+    GameState game;
+    if (!init_game(&game)) {
+        printf("Failed to initialize game!\n");
+        return 1;
+    }
+    
+    printf("Controls: WASD to move, Mouse to look, ESC to quit\n");
+    printf("Raycaster is running!\n");
+    
+    game_loop(&game);
+    cleanup_game(&game);
+    
+    return 0;
+}
