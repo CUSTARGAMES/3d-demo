9c20c08de7e0c21b86153cc9dffeaba9c0a633db
saadmann <mtaslimaroja@gmail.com>
2026-05-24 17:11:30 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
+#define SDL_MAIN_HANDLED
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include "raycast.h"
+
+int main(int argc, char* argv[]) {
+    printf("Starting DOOM-Style Raycaster...\n");
+    
+    GameState game;
+    if (!init_game(&game)) {
+        printf("Failed to initialize game!\n");
+        return 1;
+    }
+    
+    printf("Controls: WASD to move, Arrow Keys to look, ESC to quit\n");
+    printf("Raycaster is running!\n");
+    
+    game_loop(&game);
+    cleanup_game(&game);
+    
+    return 0;
+}
 
 int main(int argc, char* argv[]) {
     printf("Starting DOOM-Style Raycaster...\n");
